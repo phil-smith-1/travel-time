5fad5a38dc022a8a140e01d181b0789015dc3f76
Phil smith <pcsmith86@gmail.com>
2017-03-19 13:11:09 +0000

    updated add_postcode js and fixed postcode form labelling

--- public/js/add_postcode.js
+++ public/js/add_postcode.js
@@ -1,4 +1,4 @@
 function addPostcodeForm() {
 	var div = document.getElementById("postcodes")
-	var formHtml = $.ajax({ url: '/postcode-form', success: function(data) { div.insertAdjacentHTML('beforeend', data); } });
+	$.ajax({ url: '/postcode-form', success: function(data) { div.insertAdjacentHTML('beforeend', data); } });
 }
