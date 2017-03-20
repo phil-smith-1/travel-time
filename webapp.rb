f6b33a1f9f46b3e3409c4005774a510c1d3296a5
Phil smith <pcsmith86@gmail.com>
2017-03-20 01:32:35 +0000

    Added config file

--- webapp.rb
+++ webapp.rb
@@ -1,17 +1,19 @@
 require 'dotenv/load'
 require 'sinatra'
 
-get '/' do
-  erb :form
-end
+class WebApp < Sinatra::Base
+  get '/' do
+    erb :form
+  end
 
-get '/postcode-form' do
-  erb :postcode_form_group, layout: false
-end
+  get '/postcode-form' do
+    erb :postcode_form_group, layout: false
+  end
 
-post '/results' do
-  @results = Result.new(params).full_data
-  erb :results
-end
+  post '/results' do
+    @results = Result.new(params).full_data
+    erb :results
+  end
 
-require_relative 'models/init'
+  require_relative 'models/init'
+end
